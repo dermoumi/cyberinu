639c48cfe3e2bbfe5f74954331132a0ae8f5002f
Dermoumi S <sdermoumi@gmail.com>
2019-05-11 18:22:25 +0000

    Finished a rough, functional version of the app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM golang:1.12
 
+ENV SLACK_TOKEN=
+
 WORKDIR $GOPATH/src/github.com/dermoumi/cyberinu
 
 COPY . .
@@ -7,4 +9,4 @@ COPY . .
 RUN go get -d -v ./...
 RUN go install -v ./...
 
-CMD tail -f /dev/null
+CMD cyberinu
--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
     "os"
+    "fmt"
     "time"
     "bytes"
     "image"
     "image/color"
     "image/png"
     "io/ioutil"
+    "net/http"
+    "mime/multipart"
 
     "github.com/golang/freetype/truetype"
     "github.com/llgcode/draw2d"
     "github.com/llgcode/draw2d/draw2dimg"
 )
+
+type SlackTokenNotSet struct{}
+
+func (SlackTokenNotSet) Error() string {
+    return "Slack token not set"
+}
 
 func makeImage() *image.RGBA {
 // Load and register the background image
@@ -66,19 +75,99 @@ func makeImage() *image.RGBA {
     return output
 }
 
-func main() {
+func makeRequest(buffer *bytes.Buffer, token string) error {
+    body := new(bytes.Buffer)
+    writer := multipart.NewWriter(body)
+
+    // Add the token
+    tokenField, err := writer.CreateFormField("token")
+    if err != nil {
+        return err
+    }
+    tokenField.Write([]byte(token))
+
+    // Add the image
+    part, err := writer.CreateFormFile("image", "inu.png")
+    if err != nil {
+        return err
+    }
+    buffer.WriteTo(part)
+
+    writerContentType := writer.FormDataContentType()
+
+    err = writer.Close()
+    if err != nil {
+        return err
+    }
+
+    request, err := http.NewRequest("POST", "https://slack.com/api/users.setPhoto", body)
+    if err != nil {
+        return err
+    }
+    request.Header.Add("User-Agent", "curl/7.64.1")
+    request.Header.Add("Accept", "*/* ")
+    request.Header.Add("Content-Type", writerContentType)
+
+    client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        return err
+    } else {
+        var bodyContent []byte
+        resp.Body.Read(bodyContent)
+        resp.Body.Close()
+        fmt.Println(resp.StatusCode)
+        fmt.Println(resp.Header)
+        fmt.Println(bodyContent)
+        fmt.Println(resp.Request.ContentLength)
+        fmt.Println(resp.Request.Header)
+    }
+
+    return nil
+}
+
+func updateSlackPicture() {
+    // Generate an image of CyberInu with time on glasses
     image := makeImage()
 
+    // Encode the image to a PNG
     buffer := bytes.NewBuffer(make([]byte, 0))
     err := png.Encode(buffer, image)
     if err != nil {
         panic(err)
     }
 
-    file, err := os.Create("out.png")
+    // Retrieve slack token
+    var token string
+    if len(os.Args) >= 2 {
+        // Try to retrieve token from passed arguments first
+        token = os.Args[1]
+    }
+    if token == "" {
+        // If token not available in passed arguments
+        // try to retrieve it from environment variables
+        token = os.Getenv("SLACK_TOKEN")
+    }
+    if token == "" {
+        // Slack token not set. Abort...
+        panic(SlackTokenNotSet{})
+    }
+
+    // Make request to update slack profile picture
+    err = makeRequest(buffer, token)
     if err != nil {
         panic(err)
     }
-    defer file.Close()
-    buffer.WriteTo(file)
+}
+
+func main() {
+    aminute, err := time.ParseDuration("1m")
+    if err != nil {
+        panic(err)
+    }
+
+    for {
+        updateSlackPicture()
+        time.Sleep(aminute)
+    }
 }
